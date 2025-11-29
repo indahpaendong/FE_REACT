a1a3cbb52dd4da901cc9bb88c7f5f5494c7741fd
indahpaendong <indahpaendong026@student.unsrat.ac.id>
2025-11-30 02:43:58 +0800

    tes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./assets/style.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./assets/HomePage.css";
 
 const HomePage = () => {
   const [posts, setPosts] = useState([]);
